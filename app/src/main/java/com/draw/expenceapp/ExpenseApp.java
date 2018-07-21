39d2e1605dca925d9fadb40d78d5761520ea565e
Your Name <harjeet17@navgurukul.org>
2018-07-21 10:56:01 +0530

    Navgurukul Expense

---
+++ app/src/main/java/com/draw/expenceapp/ExpenseApp.java
@@ -0,0 +1,25 @@
+package com.draw.expenceapp;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ExpenseApp extends AppCompatActivity {
+
+    TextView txt;
+    Typeface myFont;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense_app);
+
+        txt = findViewById(R.id.name);
+        myFont = Typeface.createFromAsset(this.getAssets(),"fonts/abc.ttf");
+        txt.setTypeface(myFont);
+    }
+}
---
+++ app/src/main/java/com/draw/expenceapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.draw.expenceapp;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView, textView2;
+    Typeface myFont;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text);
+        textView2 = findViewById(R.id.text1);
+        myFont = Typeface.createFromAsset(this.getAssets(),"fonts/abc.ttf");
+        textView.setTypeface(myFont);
+        textView2.setTypeface(myFont);
+
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),ExpenseApp.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
+    }
+}
